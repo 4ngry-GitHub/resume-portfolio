cbbd8b66d59e0c1436ff759e9608a66c2bd11d81
Daniel <d.nwosu@lampalampa.net>
2024-06-27 12:38:14 +0300

    Fixed background and footer.

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,26 @@ import NavBar from './components/NavBar.vue';
 import Footer from './components/FooterBar.vue';
 </script>
 
-<template>
+<!-- <template>
 	<div class="flex flex-col h-screen overflow-hidden">
 		<NavBar />
 		<main class="flex-1 overflow-y-scroll">
 			<router-view></router-view>
 		</main>
 		<Footer />
+	</div>
+</template> -->
+<template>
+	<div class="flex flex-col min-h-screen">
+		<div class="flex-shrink-0">
+			<NavBar />
+		</div>
+		<main class="flex-1 overflow-y-auto">
+			<router-view></router-view>
+		</main>
+		<div class="flex-shrink-0">
+			<Footer />
+		</div>
 	</div>
 </template>
 
