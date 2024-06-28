4f0a6e0744a1f2dbfc98e47cf62aa0816f95d37d
D.Nwosu <4ngry24@gmail.com>
2024-06-28 15:15:52 +0300

    fixed modal bg height

--- src/components/FeedbackModal.vue
+++ src/components/FeedbackModal.vue
@@ -2,7 +2,7 @@
 	<!-- Main modal -->
 	<div
 		tabindex="-1"
-		class="flex items-center overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-neutral-500 dark:bg-zinc-900 bg-opacity-90 dark:bg-opacity-90">
+		class="flex items-center overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-neutral-500 dark:bg-zinc-900 bg-opacity-90 dark:bg-opacity-90 h-screen">
 		<div class="relative p-4 w-full max-w-md max-h-full justify-center">
 			<!-- Modal content -->
 			<div class="relative bg-neutral-200 rounded-lg shadow dark:bg-zinc-700">
